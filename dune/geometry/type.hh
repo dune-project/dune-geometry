b5dd922abbdb4ab0ee477093be8c6bc60a5e9d24
Christoph Grüninger <gruenich@dune-project.org>
2011-10-04 09:31:17 +0000

    Initial revision. New Projectw with duneproject, removed src/ and added dependencies. Created type.hh according to FS#602, comment2624 by Jö.
    [[Imported from SVN: r2]]

---
+++ dune/geometry/type.hh
@@ -0,0 +1,12 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#include <cassert>
+#ifndef DUNE_GEOMETRY_TYPE_HH
+#define DUNE_GEOMETRY_TYPE_HH
+
+#define DISABLE_GEOMETRYTYPE_DEPRECATION_WARNING
+#include <dune/common/geometrytype.hh>
+#undef DISABLE_GEOMETRYTYPE_DEPRECATION_WARNING
+
+#endif // DUNE_GEOMETRY_TYPE_HH
