266f1483280ad6992d6acd12a3c097777a17c6d1
Jorrit Fahlke <joe@dune-project.org>
2011-11-09 10:54:46 +0000

    [GeometryTypeIndex] "Move" to dune-geometry.
    Kudos to Christoph Grüninger.
    
    [[Imported from SVN: r20]]

---
+++ dune/geometry/typeindex.hh
@@ -0,0 +1,12 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#include <cassert>
+#ifndef DUNE_GEOMETRY_TYPEINDEX_HH
+#define DUNE_GEOMETRY_TYPEINDEX_HH
+
+#define DISABLE_GEOMETRYTYPEINDEX_DEPRECATION_WARNING
+#include <dune/common/geometrytypeindex.hh>
+#undef DISABLE_GEOMETRYTYPEINDEX_DEPRECATION_WARNING
+
+#endif // DUNE_GEOMETRY_TYPEINDEX_HH
