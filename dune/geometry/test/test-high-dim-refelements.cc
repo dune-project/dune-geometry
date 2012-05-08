c6739800f7e73d17ed4a7713c5f6449fa26af5a7
Martin Nolte <mnolte@dune-project.org>
2012-05-08 19:29:43 +0000

    a small test for compiling high-dimensional reference elements (will consume lots of memory)
    [[Imported from SVN: r74]]

---
+++ dune/geometry/test/test-high-dim-refelements.cc
@@ -0,0 +1,21 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include <config.h>
+
+#include <iostream>
+
+#include <dune/geometry/referenceelements.hh>
+
+typedef void ctype;
+static const int dim = 5;
+
+int main ( int argc, char **argv )
+{
+  const Dune::GeometryType gt( Dune::GeometryType::cube, dim );
+  const Dune::GenericReferenceElement< ctype, dim > &refElement
+    = Dune::GenericReferenceElements< ctype, dim >::general( gt );
+
+  std::cout << "number of vertices: " << refElement.size( dim ) << std::endl;
+
+  return 0;
+}
