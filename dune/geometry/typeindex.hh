d36ecb1d78d2a611d0860261bb2d79ebdd1c90d2
Martin Nolte <mnolte@dune-project.org>
2012-08-13 21:19:17 +0000

    a generalization of the geometry type indices
    [[Imported from SVN: r214]]

--- dune/geometry/typeindex.hh
+++ dune/geometry/typeindex.hh
@@ -15,6 +15,7 @@
 
 namespace Dune
 {
+
   //! Compute per-dimension indices for geometry types
   class LocalGeometryTypeIndex
   {
@@ -24,7 +25,7 @@ namespace Dune
      * Regular geometry type are those which have a topologyId, i.e. "None" is
      * not a regular geometry type.
      */
-    inline static std::size_t regular_size(std::size_t dim)
+    static std::size_t regular_size(std::size_t dim)
     {
       // The following expression is derived from the expression for
       // GlobalGeometryTypeIndex::regular_base().  Substracting
@@ -53,7 +54,7 @@ namespace Dune
      *
      * This includes irregular geometry types such as "None".
      */
-    inline static std::size_t size(std::size_t dim)
+    static std::size_t size(std::size_t dim)
     {
       // one for "none"
       return regular_size(dim) + 1;
@@ -65,16 +66,12 @@ namespace Dune
      * Geometry types from different dimensions my get the same index. If that
      * is not what you want, maybe you should look at GlobalGeometryTypeIndex.
      */
-    inline static std::size_t index(const GeometryType &gt)
+    static std::size_t index(const GeometryType &gt)
     {
       if(gt.isNone())
-      {
         return regular_size(gt.dim());
-      }
       else
-      {
         return gt.id() >> 1;
-      }
     }
   };
 
@@ -88,7 +85,7 @@ namespace Dune
      * This ignores irregular geometry types so it is not useful in itself.
      * Have a look at base() which does include the irregular geometry types.
      */
-    inline static std::size_t regular_base(std::size_t dim)
+    static std::size_t regular_base(std::size_t dim)
     {
       // The number of regular geometry types in a given dimension is
       // 2^(dim-1).  For dim==0 this would yield 1/2 geometry types (which is
@@ -102,7 +99,7 @@ namespace Dune
      * \brief Compute the starting index for a given dimension including
      *        irregular geometry types
      */
-    inline static std::size_t base(std::size_t dim)
+    static std::size_t base(std::size_t dim)
     {
       // dim times "none"
       return regular_base(dim) + dim;
@@ -115,7 +112,7 @@ namespace Dune
      *
      * This includes irregular geometry types such as "None".
      */
-    inline static std::size_t size(std::size_t maxdim)
+    static std::size_t size(std::size_t maxdim)
     {
       return base(maxdim+1);
     }
@@ -128,11 +125,62 @@ namespace Dune
      * dimensions.  If that is not what you want, maybe you should look at
      * LocalGeometryTypeIndex.
      */
-    inline static std::size_t index(const GeometryType &gt)
+    static std::size_t index(const GeometryType &gt)
     {
       return base(gt.dim()) + LocalGeometryTypeIndex::index(gt);
     }
   };
+
+
+
+  // GeometryTypeIndex
+  // -----------------
+
+  template< int mindim, int maxdim, bool incnone = true >
+  struct GeometryTypeIndex;
+
+  template< int mindim, int maxdim >
+  class GeometryTypeMapBase< int mindim, int maxdim, false >
+  {
+    static const std::size_t begin_offset = (std::size_t( 1 ) << mindim) / std::size_t( 2 );
+    static const std::size_t end_offset = (std::size_t( 1 ) << maxdim);
+
+  public:
+    static const int min_dimension = mindim;
+    static const int max_dimension = maxdim;
+
+    static const std::size_t size = end_offset - begin_offset;
+
+    static std::size_t index ( const GeometryType &gt )
+    {
+      assert( (gt.dim() >= min_dimension) && (gt.dim() <= max_dimension) );
+      assert( !gt.isNone() );
+      const std::size_t offset = (std::size_t( 1 ) << gt.dim()) / 2;
+      return (offset - begin_offset) + (std::size_t( gt.id() ) >> 1);
+    }
+  };
+
+  template< int mindim, int maxdim >
+  class GeometryTypeIndex< int mindim, int maxdim, true >
+  {
+    typedef GeometryTypeIndex< mindim, maxdim, false > BasicIndex;
+
+  public:
+    static const int min_dimension = mindim;
+    static const int max_dimension = maxdim;
+
+    static const std::size_t size = BasicIndex::size + (max_dimension - min_dimension);;
+
+    static std::size_t index ( const GeometryType &gt )
+    {
+      assert( (gt.dim() >= min_dimension) && (gt.dim() <= max_dimension) );
+      if( gt.isNone() )
+        return BasicIndex::size + (gt.dim() - min_dimension);
+      else
+        return BasicIndex::index( gt );
+    }
+  };
+
 } // namespace Dune
 
 #endif // DUNE_GEOMETRY_TYPEINDEX_HH
