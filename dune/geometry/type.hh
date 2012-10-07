35c0f0c21fcd4c5e8616a37968b3b265124375a8
Oliver Sander <sander@dune-project.org>
2012-10-07 16:15:17 +0000

    language
    [[Imported from SVN: r321]]

--- dune/geometry/type.hh
+++ dune/geometry/type.hh
@@ -210,7 +210,7 @@ namespace Dune
 
     /** \brief Construct the correct geometry type given the dimension and the number of vertices
      *  \note This code only works up to dimension 3.
-     *        In higher dimensions the number of vertices does not uniquely identify the type of polyhedral.
+     *        In higher dimensions the number of vertices does not uniquely identify the type of polyhedron.
      */
     void makeFromVertices(unsigned int dim, unsigned int vertices)
     {
