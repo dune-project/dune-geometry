541efae4f00caa48935c27b774db26fb9f565d81
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-05-29 18:11:52 +0200

    Add composite quadrature rules for simplex elements
    This short file implements a general mechanism for constructing
    composite quadrature rules.  It takes some quadrature rule
    and an integer refinement level as the input.  The reference
    simplex is the refined uniformly as prescribed by the desired
    level.  The given input quadrature rule is then used on
    each subelement.
    
    Currently this only works for simplices.  I'll try to implement
    it also for other elements, but there are some fixes in the
    virtual refinement code needed first.

---
+++ dune/geometry/quadraturerules/compositequadraturerule.hh
@@ -0,0 +1,66 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_GEOMETRY_COMPOSITE_QUADRATURE_RULE_HH
+#define DUNE_GEOMETRY_COMPOSITE_QUADRATURE_RULE_HH
+
+/** \file
+ * \brief Construct composite quadrature rules from other quadrature rules
+ */
+
+#include <dune/geometry/quadraturerules.hh>
+#include <dune/geometry/virtualrefinement.hh>
+
+namespace Dune {
+
+  /** \brief Construct composite quadrature rules from other quadrature rules
+   *
+   * \tparam ctype Type used for coordinates and quadrature weights
+   * \tparam dim Dimension of the reference element
+   */
+  template <class ctype, int dim>
+  class CompositeQuadratureRule
+      : public Dune::QuadratureRule<ctype,dim>
+  {
+    public:
+    /** \brief Construct composite quadrature rule
+     * \param quad Base quadrature rule.  Element type of this rule must be simplex
+     * \param refinement Number of uniform refinement steps
+     */
+    CompositeQuadratureRule(const Dune::QuadratureRule<ctype,dim>& quad, int refinement)
+      : QuadratureRule<ctype,dim>(quad.type(), quad.order())
+    {
+      // Currently only works for simplices, because we are using the StaticRefinement
+      assert(quad.type().isSimplex());
+
+      typedef Dune::StaticRefinement<Dune::GenericGeometry::SimplexTopology<dim>::type::id,
+                                     ctype,
+                                     Dune::GenericGeometry::SimplexTopology<dim>::type::id,
+                                     dim> Refinement;
+      typedef typename Refinement::ElementIterator eIterator;
+
+      ctype volume = Dune::ReferenceElements<ctype,dim>::general(quad.type()).volume();
+
+      eIterator eSubEnd = Refinement::eEnd(refinement);
+      eIterator eSubIt  = Refinement::eBegin(refinement);
+
+      for (; eSubIt != eSubEnd; ++eSubIt) {
+
+        // Percentage of the overall volume of this subelement
+        ctype volumeFraction = eSubIt.geometry().volume() / volume;
+
+        for (size_t i=0; i<quad.size(); i++) {
+
+          this->push_back(Dune::QuadraturePoint<ctype,dim>(eSubIt.geometry().global(quad[i].position()),
+                                                           volumeFraction*quad[i].weight()));
+
+        }
+
+      }
+
+    }
+
+  };
+
+}
+
+#endif   // DUNE_GEOMETRY_COMPOSITE_QUADRATURE_RULE_HH
