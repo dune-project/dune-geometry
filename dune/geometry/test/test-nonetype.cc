030a309cb882ac502fc6c0475392dc56b61e5e1f
Christian Engwer <christi@dune-project.org>
2014-08-11 16:13:37 +0200

    extend test-nonetype

--- dune/geometry/test/test-nonetype.cc
+++ dune/geometry/test/test-nonetype.cc
@@ -14,6 +14,18 @@ int main ( int argc, char **argv )
     Dune::GeometryType gt2;
     gt2.makeNone( dim );
 
+    if ( ! gt1.isNone() )
+    {
+      fail = 1;
+      std::cerr << "Geometry types 'none' for dim " << dim << " has wrong constructor " << std::endl;
+    }
+
+    if ( ! gt2.isNone() )
+    {
+      fail = 1;
+      std::cerr << "Geometry types 'none' for dim " << dim << " fails using makeNone " << std::endl;
+    }
+
     if( gt1 != gt2 )
     {
       fail = 1;
