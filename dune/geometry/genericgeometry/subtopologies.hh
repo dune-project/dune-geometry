d48a651e83318c72b1885c813bea9de6ba437482
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-11-04 09:24:49 +0100

    remove now unused GenericGeometry::SubTopology
    The only use in the core was in dune-localfunctions and has been replaced by the
    reference element and IfTopology.

--- dune/geometry/genericgeometry/subtopologies.hh
+++ dune/geometry/genericgeometry/subtopologies.hh
@@ -22,9 +22,6 @@ namespace Dune
     template< class Topology, unsigned int codim >
     struct Size;
 
-    template< class Topology, unsigned int codim, unsigned int i >
-    struct SubTopology;
-
 
 
     // Size
@@ -131,129 +128,6 @@ namespace Dune
 
 
 
-    // SubTopology
-    // -----------
-
-    template< class Topology, unsigned int dim, unsigned int codim, unsigned int i >
-    class SubTopologyImpl;
-
-    template< unsigned int dim, unsigned int codim, unsigned int i >
-    class SubTopologyImpl< Point, dim, codim, i >
-    {
-      typedef Point Topology;
-      static_assert((dim == Topology :: dimension), "Wrong dimension");
-      static_assert((codim <= dim), "Invalid codimension");
-      static_assert((i < Size< Topology, codim > :: value), "Invalid subentity index");
-
-    public:
-      typedef Topology type;
-    };
-
-    template< class BaseTopology, unsigned int dim, unsigned int codim, unsigned int i >
-    class SubTopologyImpl< Prism< BaseTopology >, dim, codim, i >
-    {
-      typedef Prism< BaseTopology > Topology;
-      static_assert((dim == Topology :: dimension), "Wrong dimension");
-      static_assert((codim <= dim), "Invalid codimension");
-      static_assert((i < Size< Topology, codim > :: value), "Invalid subentity index");
-
-      enum { m = Size< BaseTopology, codim-1 > :: value };
-      enum { n = Size< BaseTopology, codim > :: value };
-
-      enum { s = (i < n+m ? 0 : 1) };
-
-      template< bool >
-      struct PrismSub
-      {
-        typedef Prism< typename SubTopology< BaseTopology, codim, i > :: type > type;
-      };
-
-      template< bool >
-      struct BaseSub
-      {
-        typedef typename SubTopology< BaseTopology, codim-1, i-(n+s*m) > :: type type;
-      };
-
-    public:
-      typedef typename std::conditional< (i < n), PrismSub<true>, BaseSub<false> > :: type :: type type;
-    };
-
-    template< class BaseTopology, unsigned int dim, unsigned int i >
-    class SubTopologyImpl< Prism< BaseTopology >, dim, 0, i >
-    {
-      typedef Prism< BaseTopology > Topology;
-      static_assert((dim == Topology :: dimension), "Wrong dimension");
-      static_assert((i < Size< Topology, 0 > :: value), "Invalid subentity index");
-    public:
-      typedef Topology type;
-    };
-
-    template< class BaseTopology, unsigned int dim, unsigned int i >
-    class SubTopologyImpl< Prism< BaseTopology >, dim, dim, i >
-    {
-      typedef Prism< BaseTopology > Topology;
-      static_assert((dim == Topology :: dimension), "Wrong dimension");
-      static_assert((i < Size< Topology, dim > :: value), "Invalid subentity index");
-    public:
-      typedef Point type;
-    };
-
-    template< class BaseTopology, unsigned int dim, unsigned int codim, unsigned int i >
-    class SubTopologyImpl< Pyramid< BaseTopology >, dim, codim, i >
-    {
-      typedef Pyramid< BaseTopology > Topology;
-      static_assert((dim == Topology :: dimension), "Wrong dimension");
-      static_assert((codim <= dim), "Invalid codimension" );
-      static_assert((i < Size< Topology, codim > :: value), "Invalid subentity index");
-
-      enum { m = Size< BaseTopology, codim-1 > :: value };
-
-      template< bool >
-      struct BaseSub
-      {
-        typedef typename SubTopology< BaseTopology, codim-1, i > :: type type;
-      };
-
-      template< bool >
-      struct PyramidSub
-      {
-        typedef Pyramid< typename SubTopology< BaseTopology, codim, i-m > :: type > type;
-      };
-
-    public:
-      typedef typename std::conditional< (i < m), BaseSub<true>, PyramidSub<false> > :: type :: type type;
-    };
-
-    template< class BaseTopology, unsigned int dim, unsigned int i >
-    class SubTopologyImpl< Pyramid< BaseTopology >, dim, 0, i >
-    {
-      typedef Pyramid< BaseTopology > Topology;
-      static_assert((dim == Topology :: dimension), "Wrong dimension");
-      static_assert((i < Size< Topology, 0 > :: value), "Invalid subentity index");
-
-    public:
-      typedef Topology type;
-    };
-
-    template< class BaseTopology, unsigned int dim, unsigned int i >
-    class SubTopologyImpl< Pyramid< BaseTopology >, dim, dim, i >
-    {
-      typedef Pyramid< BaseTopology > Topology;
-      static_assert((dim == Topology :: dimension), "Wrong dimension");
-      static_assert((i < Size< Topology, dim > :: value), "Invalid subentity index");
-
-    public:
-      typedef Point type;
-    };
-
-    template< class Topology, unsigned int codim, unsigned int i >
-    struct SubTopology
-    {
-      typedef typename SubTopologyImpl< Topology, Topology :: dimension, codim, i > :: type type;
-    };
-
-
-
     /** \brief Compute the topology id of a given subentity
      *
      * \param topologyId Topology id of entity
@@ -262,7 +136,6 @@ namespace Dune
      * \param i Number of the subentity that we are interested in
      */
     unsigned int subTopologyId ( unsigned int topologyId, int dim, int codim, unsigned int i );
-
 
 
 
