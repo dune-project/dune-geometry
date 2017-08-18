64d30174a6858a4d73468de160acf73816720991
Steffen Müthing <muething@dune-project.org>
2017-08-18 14:31:04 +0200

    [tests] Test GeometryType factory functions

--- dune/geometry/test/test-constexpr-geometrytype.cc
+++ dune/geometry/test/test-constexpr-geometrytype.cc
@@ -7,5 +7,6 @@
 int main ( int argc, char **argv )
 {
   constexpr auto gt_none_1 = Dune::GeometryType(2);
-  return not std::integral_constant<bool,gt_none_1.isNone()>{};
+  constexpr auto gt_none_2 = Dune::GeometryType::None(2);
+  return not std::integral_constant<bool,gt_none_1.isNone() and gt_none_1 == gt_none_2>{};
 }
