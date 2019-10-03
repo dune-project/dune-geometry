9d0ad6b02cc1490dd154206634bfef77779881a9
Henrik Stolzmann <henrik.stolzmann@mailbox.tu-dresden.de>
2019-10-03 14:08:57 +0200

    recovered intfunc.** and CMakeLists.txt

---
+++ dune/geometry/quadraturerules/intfunc.cc
@@ -0,0 +1,29 @@
+#include "config.h"
+#include <iostream>
+
+#include "intfunc.hh"
+#include "calculatesimplexrules.hh"
+
+#include <dune/geometry/quadraturerules.hh>
+
+int main()
+{
+  using namespace Dune;
+  const int dim = 1;
+  const int order = 5;
+
+  std::cout << "  dim = "<<dim <<", order = " <<order<<" \n";
+  // get rule
+  const auto& quadRuleOld = QuadratureRules<double,dim>::rule(GeometryType::simplex, order);
+
+  std::cout << "--------------------\nquadRuleOld "<<quadRuleOld.size() <<" = \n";
+  //printRule(quadRuleOld);
+  std::cout << "int(f) = "<< integrate<double,dim>(f<double,dim>, quadRuleOld) <<"\n";
+
+  // get new rule
+  const auto& quadRule = QuadratureRules<double,dim>::rule(GeometryType::simplex, order, Dune::QuadratureType::GaussJacobiArbitraryOrder);
+  std::cout << "--------------------\nQuadRule "<<quadRule.size() <<" = \n";
+  //printRule(quadRule);
+
+  std::cout << "int(f) = "<< integrate<double,dim>(f<double,dim>, quadRule) <<"\n";
+}
---
+++ dune/geometry/quadraturerules/intfunc.hh
@@ -0,0 +1,36 @@
+#include <dune/geometry/quadraturerules.hh>
+#include <functional>
+#include <numeric>
+#include <vector>
+
+#include "quadraturerules.cc"
+
+template<typename ct, int dim>
+ct f(Dune::FieldVector<ct,dim> const& vec)
+{
+  auto const& x = vec[0];
+  return x*x;
+}
+
+template<typename ct, int dim>
+ct integrate(std::function<ct(Dune::FieldVector<ct,dim> const&)> const& f, Dune::QuadratureRule<ct,dim> const& rule)
+{
+  ct sum =0;
+  auto applyFunctionAndSumUp = [f](ct const& sum, auto const& quadpoint) {
+                       return sum + f(quadpoint.position())*quadpoint.weight();
+                   };
+  return std::accumulate(rule.begin(), rule.end(), sum, applyFunctionAndSumUp);
+}
+template<typename ct,int dim>
+void printRule(Dune::QuadratureRule<ct,dim> const& rule)
+{
+  for(size_t i=0;i<rule.size();++i)
+  {
+    std::cout << "position = ";
+    for(size_t j=0;j<rule[i].position().size();++j)
+    {
+      std::cout << rule[i].position()[j] << ",";
+    }
+    std::cout << "     weight = "<< rule[i].weight() << "\n";
+  }
+}
