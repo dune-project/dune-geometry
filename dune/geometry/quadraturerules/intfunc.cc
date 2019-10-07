b314dce1b406cda1282e442c6c82b620fbb75663
Henrik Stolzmann <henrik.stolzmann@mailbox.tu-dresden.de>
2019-10-07 22:10:46 +0200

    Works now. Just have to add documentation and the new error

--- dune/geometry/quadraturerules/intfunc.cc
+++ dune/geometry/quadraturerules/intfunc.cc
@@ -2,15 +2,15 @@
 #include <iostream>
 
 #include "intfunc.hh"
-#include "calculatesimplexrules.hh"
+#include "jacobi_n_0.hh"
 
 #include <dune/geometry/quadraturerules.hh>
 
 int main()
 {
   using namespace Dune;
-  const int dim = 1;
-  const int order = 5;
+  const int dim = 5;
+  const int order = 9;
 
   std::cout << "  dim = "<<dim <<", order = " <<order<<" \n";
   // get rule
@@ -21,9 +21,15 @@ int main()
   std::cout << "int(f) = "<< integrate<double,dim>(f<double,dim>, quadRuleOld) <<"\n";
 
   // get new rule
-  const auto& quadRule = QuadratureRules<double,dim>::rule(GeometryType::simplex, order, Dune::QuadratureType::GaussJacobiArbitraryOrder);
+  const auto& quadRule = QuadratureRules<double,dim>::rule(GeometryType::simplex, order, Dune::QuadratureType::GaussJacobi_n_0);
   std::cout << "--------------------\nQuadRule "<<quadRule.size() <<" = \n";
   //printRule(quadRule);
 
   std::cout << "int(f) = "<< integrate<double,dim>(f<double,dim>, quadRule) <<"\n";
+
+  std::cout << "\n----------------------------------------------------------\n";
+  const int alpha =1;
+  std::cout << "jacobi alpha ="<< alpha <<"\n";
+  const auto& rule = JacobiNQuadratureRule1D<double>(order, alpha);
+  const auto& rule1 = JacobiNQuadratureRule1D<int>(order, alpha);
 }
--- dune/geometry/quadraturerules/jacobi_n_0.hh
+++ dune/geometry/quadraturerules/jacobi_n_0.hh
@@ -2,6 +2,7 @@
 #define CALCULATE_SIMPLEX_RULES_H
 
 #include <vector>
+#include <type_traits>
 
 #include <dune/common/fvector.hh>
 #include <dune/common/fmatrix.hh>
@@ -11,6 +12,7 @@
 #include <dune/common/dynvector.hh>
 #include <dune/common/dynmatrixev.hh>
 
+
 namespace Dune {
 
   /*
@@ -19,6 +21,7 @@ namespace Dune {
    * For more information see the comment in `tensorproductquadrature.hh`
    *
    */
+
   template<typename ct>
   class JacobiNQuadratureRule1D : public QuadratureRule<ct,1>
   {
@@ -27,7 +30,7 @@ namespace Dune {
     enum { dim=1 };
 
     ~JacobiNQuadratureRule1D(){}
-  private:
+  //private:
 
     typedef QuadratureRule<ct, dim> Rule;
 
@@ -41,10 +44,11 @@ namespace Dune {
     {
       if (unsigned(order) > maxOrder())
         DUNE_THROW(QuadratureOrderOutOfRange, "Quadrature rule " << order << " not supported!");
-      auto&& rule = jacobiNodesWeights(order,alpha);
+      auto&& rule = decideRule(order,alpha);
       for(unsigned int i=0; i< rule.size(); i++)
         this->push_back(rule[i]);
       this->delivered_order = 2*rule.size()-1;
+
     }
 
     static unsigned maxOrder()
@@ -52,8 +56,56 @@ namespace Dune {
       return 127; // can be changed
     }
 
+    QuadratureRule<ct,1> decideRule(int const degree, int const alpha)
+    {
+      const auto maxOrder = std::min(   unsigned(GaussQuadratureRule1D<ct>::highest_order),
+                              std::min( unsigned(Jacobi1QuadratureRule1D<ct>::highest_order),
+                                        unsigned(Jacobi2QuadratureRule1D<ct>::highest_order))
+                              );
+      return unsigned(degree) < maxOrder ? decideRuleExponent(degree,alpha) : UseLapackOrError<ct>(degree, alpha);
+    }
+
+    template<typename type, std::enable_if_t<!(std::is_floating_point<type>::value)>* = nullptr>
+    QuadratureRule<ct,1> UseLapackOrError( int const degree, int const alpha)
+    {
+      // TODO make an Error
+      DUNE_THROW(QuadratureOrderOutOfRange, "Quadrature rule " << degree << " not supported!");
+    }
+
+    template<typename type, std::enable_if_t<std::is_floating_point<type>::value>* = nullptr>
+    QuadratureRule<ct,1> UseLapackOrError( int const degree, int const alpha)
+    {
+      return jacobiNodesWeights(degree, alpha);
+    }
+
+    QuadratureRule<ct,1> decideRuleExponent(int const degree, int const alpha)
+    {
+      switch(alpha)
+      {
+        case 0 :  return QuadratureRules<ct,1>::rule(GeometryTypes::line, degree, QuadratureType::GaussLegendre);
+                  // the weights are not correct for the use of quadrature rules
+        case 1 :  {
+                    auto&& rule = QuadratureRules<ct,1>::rule(GeometryTypes::line, degree, QuadratureType::GaussJacobi_1_0);
+                    QuadratureRule<ct,1> quadratureRule;
+                    quadratureRule.reserve(rule.size());
+                    for( auto qpoint : rule )
+                      quadratureRule.push_back(QuadraturePoint<ct,1>(qpoint.position(),ct(0.5)*qpoint.weight()));
+                    return quadratureRule;
+                  }
+        case 2 :  {
+                    auto&& rule = QuadratureRules<ct,1>::rule(GeometryTypes::line, degree, QuadratureType::GaussJacobi_2_0);
+                    QuadratureRule<ct,1> quadratureRule;
+                    quadratureRule.reserve(rule.size());
+                    for( auto qpoint : rule )
+                      quadratureRule.push_back(QuadraturePoint<ct,1>(qpoint.position(),ct(0.25)*qpoint.weight()));
+                    return quadratureRule;
+                  }
+        default : return UseLapackOrError<ct>(degree,alpha);
+      }
+    }
+
     // computes the nodes and weights for the weight function (1-x)^alpha, which are exact for given polynomials with degree "degree"
-    QuadratureRule<ct,1> jacobiNodesWeights(int const degree, int const alpha=0 )
+    QuadratureRule<ct,1> jacobiNodesWeights(int const degree, int const alpha)
     {
       using std::sqrt;
 
@@ -97,6 +149,7 @@ namespace Dune {
       }
 
       return quadratureRule;
+
     }
 
   };
