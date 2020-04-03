bb461d9673ca6688b4012e4471ebd440188eddc1
dedner <a.s.dedner@warwick.ac.uk>
2020-04-03 16:41:46 +0100

    added some python tests

---
+++ dune/python/test/geometrytype.py
@@ -0,0 +1,20 @@
+from dune.geometry import *
+
+# make sure t can be reconstructed from str(t)
+for t in (vertex, line, triangle, quadrilateral, tetrahedron, pyramid, prism, hexahedron):
+    assert GeometryType(str(t)) == t
+
+for d in range(10):
+    assert GeometryType(str(simplex(d))) == simplex(d)
+    assert GeometryType(str(cube(d))) == cube(d)
+    assert GeometryType(str(none(d))) == none(d)
+
+# make sure simplices with special names can be constructed by general mechanism
+for d, t in enumerate((vertex, line, triangle, tetrahedron)):
+    assert GeometryType("simplex(" + str(d) + ")") == t
+    assert GeometryType("general( 0, " + str(d) + ")") == t
+
+# make sure cube with special names can be constructed by general mechanism
+for d, t in enumerate((vertex, line, quadrilateral, hexahedron)):
+    assert GeometryType("cube(" + str(d) + ")") == t
+    assert GeometryType("general( " + str(2**d-1) + ", " + str(d) + ")") == t
---
+++ dune/python/test/refelement.py
@@ -0,0 +1,40 @@
+from dune.geometry import referenceElement
+from dune.geometry import vertex, line, triangle, quadrilateral, tetrahedron, pyramid, prism, hexahedron, none
+
+def test(r):
+    for codim in range(r.dimension+1):
+        types = r.types(codim)
+        if len(types) != r.size(codim):
+            raise Exception("types tuple has wrong size")
+        for i in range(len(types)):
+            if types[i] != r.type(i, codim):
+                raise Exception("types tuple has wrong content")
+
+    for codim in range(r.dimension+1):
+        positions = r.positions(codim)
+        if len(positions) != r.size(codim):
+            raise Exception("positions tuple has wrong size")
+        for i in range(len(positions)):
+            if positions[i] != r.position(i, codim):
+                raise Exception("positions tuple has wrong content")
+
+    if r.dimension > 0:
+        normals = r.integrationOuterNormals
+        if len(normals) != r.size(1):
+            raise Exception("integrationOuterNormals has wrong size")
+        for i in range(len(normals)):
+            if normals[i] != r.integrationOuterNormal(i):
+                raise Exception("integrationOuterNormals has wrong content")
+
+test(referenceElement(vertex))
+test(referenceElement(line))
+test(referenceElement(triangle))
+test(referenceElement(quadrilateral))
+test(referenceElement(tetrahedron))
+test(referenceElement(pyramid))
+test(referenceElement(prism))
+test(referenceElement(hexahedron))
+
+for dim in range(4):
+    if referenceElement(none(dim)) is not None:
+        raise Exception("got reference element for geometry type none")
