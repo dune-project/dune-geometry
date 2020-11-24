564973fd123f57107d20125d97fc0665c970d75d
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-11-24 10:59:43 +0100

    Replace setup.py by package.py.

---
+++ package.py
@@ -0,0 +1,11 @@
+# Meta data
+name="dune-geometry"
+version="2.8"
+author="The Dune Core developers"
+author_email="dune@lists.dune-project.org"
+description="Geometry Transformations, Reference Elements and Quadrature Rules"
+url="https://gitlab.dune-project.org/core/dune-geometry"
+install_requires=[]
+
+# DUNE dependencies
+dune_dependencies=['dune-common']
